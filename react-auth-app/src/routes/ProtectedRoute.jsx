eaf5608cfb88ea553964af11484a81cfb708e85b
Avishek Mehta <mehtaavishek05@gmail.com>
2026-07-08 22:26:12 +0545

    Implement routing with AppRoutes and ProtectedRoute components for user authentication flow

---
+++ react-auth-app/src/routes/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+  const isAuth = localStorage.getItem("isAuth");
+
+  return isAuth
+    ? children
+    : <Navigate to="/" />;
+};
+
+export default ProtectedRoute;
